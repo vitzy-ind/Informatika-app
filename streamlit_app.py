9064f5b514e320f98616f69035ba1a8c70cb158a
vitzy-ind <satoukazuma180808@gmail.com>
2025-05-21 14:56:15 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,9 +6,7 @@ st.write("halo")
 st.image("IMG_20250521_144428.jpg", width=300)
 st.write("cats shark")
 st.sidebar.image("IMG_20250521_144428.jpg", width=200)
-st.title("Aplikasi Sederhana")
-
-st.header("Aplikasi Mengecek Nilai Genap/Ganjil")
+st.header("Genap/Ganjil")
 
 angka = st.number_input("Tulis sebuah Angka:", value=0, step=1)
 
