cf72a65bf9820c7a92ba85c2cb655cb50f22c75a
vitzy-ind <satoukazuma180808@gmail.com>
2025-05-21 14:54:04 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,13 @@ st.write("halo")
 st.image("IMG_20250521_144428.jpg", width=300)
 st.write("cats shark")
 st.sidebar.image("IMG_20250521_144428.jpg", width=200)
+st.title("Aplikasi Sederhana")
+
+st. header("Aplikasi Mengecek Nilai Genap/Ganjil")
+
+angka st.number_input("Tulis sebuah Angka:", value=0, step=1)
+
+if (angka % 2) 0:
+  st.write(f"(angka) adalah Bilangan Genap")
+else:
+  st.write(f"(angka) adalah Bilangan Ganjil")
