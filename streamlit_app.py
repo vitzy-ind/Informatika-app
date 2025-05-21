ab9f1db97b54dcc3736ccafbc826b5aaf06f7577
vitzy-ind <satoukazuma180808@gmail.com>
2025-05-21 15:13:13 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,6 @@ st.header("Genap/Ganjil")
 angka = st.number_input("Tulis sebuah Angka:", value=0, step=1)
 
 if (angka % 2) == 0:
-  st.write(f"(angka) Genap")
+  st.write(f"Genap")
 else:
-  st.write(f"(angka) Ganjil")
+  st.write(f"Ganjil")
