13d58d33c7fb2e262f1aa4026c63d21827d9aaac
vitzy-ind <satoukazuma180808@gmail.com>
2025-05-28 13:57:00 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,21 +36,3 @@ with col2:
 
     def hitung_total(harga, jumlah):
         return harga * jumlah
-
-    harga_barang = st.number_input("Masukkan Harga Barang:", value=0, step=1000)
-    jumlah_barang = st.number_input("Masukkan Jumlah Barang:", value=0, step=1)
-    total_harga = hitung_total(harga_barang, jumlah_barang)
-
-    if total_harga > 100000:
-        total_harga_diskon = total_harga - 0.05 * total_harga
-        st.write(f"Total Harga (dengan diskon): Rp. {total_harga_diskon:,.0f}")
-    else:
-        st.write(f"Total Harga: Rp. {total_harga:,.0f}")
-
-    bayar = st.number_input("Masukkan Jumlah Uang:", value=0, step=10000)
-
-    if bayar >= total_harga:
-        kembalian_uang = bayar - total_harga
-        st.write(f"Uang Kembalian: Rp. {kembalian_uang:,.0f}")
-    else:
-        st.write("Uang yang anda bayarkan kurang ")
