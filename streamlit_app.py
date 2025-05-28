4d705d0025f0d050fb7c3d318afe2615053c1ef7
vitzy-ind <satoukazuma180808@gmail.com>
2025-05-28 13:57:46 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,22 +17,3 @@ else:
 
 st.sidebar.write("gtwsih")
 
-st.title("Aplikasi Sederhana")
-
-# Menggunakan layout kolom
-col1, col2 = st.columns(2)
-
-with col1:
-    st.header("Aplikasi Mengecek Nilai Genap/Ganjil")
-    angka = st.number_input("Tulis sebuah Angka:", value=0, step=1)
-
-    if (angka % 2) == 0:
-        st.write(f"{angka} adalah Bilangan Genap")
-    else:
-        st.write(f"{angka} adalah Bilangan Ganjil")
-
-with col2:
-    st.header("Aplikasi menghitung Total Belanja")
-
-    def hitung_total(harga, jumlah):
-        return harga * jumlah
